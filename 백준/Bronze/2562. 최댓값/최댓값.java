fcda5a9ce5be271075250c35f43656d5ff287ae9
hyeonbin <90017767+hyeonbinn@users.noreply.github.com>
2023-08-03 23:47:39 +0900

    [Bronze III] Title: 최댓값, Time: 228 ms, Memory: 18348 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+        
+		Scanner in = new Scanner(System.in);
+        
+		int[] arr = { in.nextInt(), in.nextInt(), in.nextInt(),
+				in.nextInt(), in.nextInt(), in.nextInt(),
+				in.nextInt(), in.nextInt(), in.nextInt() };
+		
+		int max = 0;
+		int index = 0;
+        int count = 0;
+        
+		for (int value : arr) {
+	        count++;
+    
+	        if(value > max) {
+    	        max = value;
+                index = count;
+	        }
+        }
+        
+		System.out.print(max + "\n" + index);
+	}
+}
