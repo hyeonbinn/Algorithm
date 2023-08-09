e3f61b5053223dda9300133bdadd5b61164066fe
hyeonbin <90017767+hyeonbinn@users.noreply.github.com>
2023-08-09 14:53:48 +0900

    [Bronze II] Title: 공 바꾸기, Time: 248 ms, Memory: 18532 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,29 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-
+	public static void main(String[] args){
+    	
         Scanner in = new Scanner(System.in);
-
-        int N = in.nextInt();
-        int arr[] = new int[N];
-        int M = in.nextInt();
-        int temp;
-
-        for(int i = 0; i < arr.length; i++) {
-            arr[i] = i + 1;                     
-        }    
         
-        for(int j = 0; j < M; j++) {
-            int I = in.nextInt();
-            int J = in.nextInt();
-
-            temp = arr[I-1];
-            arr[I-1] = arr[J-1];
-            arr[J-1] = temp;
+        int N = in.nextInt();
+        int M = in.nextInt();
+        
+        int array[] = new int[N];
+        
+        int temp;
+        
+        for(int i = 0; i < array.length; i++) {
+        	array[i] = i + 1; //인덱스 번호와 바구니 수 맞추기 위해 +1
         }
-        for(int k = 0; k <arr.length; k++) {
-            System.out.print(arr[k] + " ");
+        
+       	for (int j = 0; j < M; j++) {
+        	int I = in.nextInt();
+            int J = in.nextInt();
+            
+            temp = array[I-1];
+            array[I-1] = array[J-1];
+            array[J-1] = temp;
+        }
+        
+        for (int k = 0; k < array.length; k++) {
+        	System.out.print(array[k] + " " );
         }
     }
 }
