0d04b59d3341c7bcb55075e4eb00db6407aff908
hyeonbin <90017767+hyeonbinn@users.noreply.github.com>
2024-03-04 15:11:39 +0900

    [Silver II] Title: 유기농 배추, Time: 396 ms, Memory: 31744 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -0,0 +1,71 @@
+//package org.example.DfsBfs;
+
+import java.util.Scanner;
+
+public class Main {
+
+    //1을 만나면 상하좌우 탐색. 또 1을 만나면 count수 증가하지 않기.
+    static int T;
+    static int M,N,K;
+    static int[][] Graph;
+    static boolean[][] Visited; //방문 여부
+    static int count;
+    static int[] dx = {0, -1, 0, 1}; //좌우
+    static int[] dy = {-1, 0, 1, 0}; //상하
+
+    static void dfs(int x, int y) {
+        Visited[x][y] = true;
+
+        for(int i = 0; i < 4; i++) { //상하좌우 4번
+            int nr = x + dx[i]; //새로운 행 좌표
+            int nc = y + dy[i]; //새로운 열 좌표
+
+            if(nr < 0 || nr > M-1 || nc < 0 || nc > N-1) //범위 벗어나면 continue
+                continue;
+            if(Visited[nr][nc]) //방문했으면 continue
+                continue;
+            if(Graph[nr][nc] != 1) //1이면 continue
+                continue;
+
+            dfs(nr,nc);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        T = in.nextInt();
+
+        for(int i = 0; i < T; i++) { //테스트 케이스만큼 반복
+
+            count = 0;
+
+            M = in.nextInt();
+            N = in.nextInt();
+            K = in.nextInt();
+
+            //M,N 입력받은 후 배열 크기 선언
+            Graph = new int[M][N];
+            Visited = new boolean[M][N];
+
+            for (int j = 0; j < K; j++) {
+                int cx = in.nextInt();
+                int cy = in.nextInt();
+
+                Graph[cx][cy] = 1; //테스트 케이스만큼 두 개씩 입력 받아 배열을 1로 세팅
+            }
+
+            for(int x = 0; x < M; x++) {
+                for(int y = 0; y < N; y++) {
+                    if (Graph[x][y] == 1 && !Visited[x][y]) {
+                        dfs(x,y);
+                        count++;
+                    }
+                }
+            }
+
+            System.out.println(count);
+        }
+    }
+}
