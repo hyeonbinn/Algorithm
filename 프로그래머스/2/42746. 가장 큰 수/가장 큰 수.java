47fe835a2944bf2716df279edbb58d6ca23e9b54
hyeonbin <90017767+hyeonbinn@users.noreply.github.com>
2024-07-16 16:58:18 +0900

    [level 2] Title: 가장 큰 수, Time: 2948.21 ms, Memory: 412 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+
+        // 숫자를 문자열로 변환
+        String[] num = new String[numbers.length];
+        for(int i = 0; i < numbers.length; i++) {
+            num[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(num, (o1, o2) -> (o2 + o1).compareTo(o1 + o2)); //조합했을 때 큰 조합으로 정렬
+
+        String answer = "";
+        for(int i = 0; i < num.length; i++) {
+            answer += num[i];
+        }
+
+        if(answer.charAt(0) == '0') { //모두 0인 경우 0 return
+            return "0";
+        }
+
+        return answer;
+    }
+}
