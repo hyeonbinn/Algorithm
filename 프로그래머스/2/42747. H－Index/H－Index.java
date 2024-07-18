79dc684dbcf406f783b5f8250c3f44ca51ddf8db
hyeonbin <90017767+hyeonbinn@users.noreply.github.com>
2024-07-18 10:58:13 +0900

    [level 2] Title: H-Index, Time: 0.98 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,20 +2,16 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] citations) {
-
         Arrays.sort(citations);
+        int n = citations.length;
 
-        int hIndex = 0;
-
-        for (int i = 0; i < citations.length; i++) {
-            int h = citations.length - i; 
-
+        for (int i = 0; i < n; i++) {
+            int h = n - i;
             if (citations[i] >= h) {
-                hIndex = h;
-                break;
+                return h;
             }
         }
 
-        return hIndex;
+        return 0;
     }
 }
