45a784a5a4c3e941568665de0fd9f93e65afe61a
hyeonbin <90017767+hyeonbinn@users.noreply.github.com>
2024-07-24 02:14:12 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -10,12 +10,12 @@ class Solution {
                 if (height < 3) continue; //높이가 3 미만이면 continue
 
                 //내부 타일 가로, 세로 계산
-                int innerWidth = width - 2;
-                int innerHeight = height - 2;
+                int innerWidth = width - 2; //양옆사이드
+                int innerHeight = height - 2; //맨위, 맨아래
 
                 //innerWidth * innerHeight가 노란색 타일의 수와 일치하는지 확인
                 if (innerWidth * innerHeight == yellow) {
-                    if (width >= height) { // 가로는 세로보다 크거나 같아야함
+                    if (width >= height) { // 가로는 세로보다 크거나 같아야함 (문제 조건)
                         return new int[]{width, height};
                     } else {
                         return new int[]{height, width};
