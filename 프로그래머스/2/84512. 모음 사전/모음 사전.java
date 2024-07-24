d8e50f744638e07128ff9b8bc1e972eeea2298c0
hyeonbin <90017767+hyeonbinn@users.noreply.github.com>
2024-07-24 14:18:17 +0900

    [level 2] Title: 모음 사전, Time: 20.06 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -3,10 +3,10 @@ class Solution {
     //순회하면서 해당하는 순서에 바로 return
     private static final char[] ALPHABETS = {'A', 'E', 'I', 'O', 'U'};
     private static int answer;
-    private static String currentWord;
+    private static String currentWord; //AAAAE
 
     public static int solution(String word) {
-        currentWord = word;
+        currentWord = word; //AAAAE
         answer = 0;
 
         for (char c : ALPHABETS) {
@@ -19,18 +19,18 @@ class Solution {
 
     private static boolean dfs(String str) {
         answer++;
-        if (str.equals(currentWord)) {
+        if (str.equals(currentWord)) { //str이 currentWord와 같으면 true 반환.
             return true;
         }
-        if (str.length() == 5) {
+        if (str.length() == 5) { //str 길이가 5가 되면 새로운 문자 추가할 수 없으므로 false 반환.
             return false;
         }
 
-        for (char c : ALPHABETS) {
-            if (dfs(str + c)) {
+        for (char c : ALPHABETS) { // dfs("AA"), dfs("AE"), dfs("AI"), dfs("AO"), dfs("AU")
+            if (dfs(str + c)) { //모음 추가해 dfs 호출
                 return true;
             }
         }
         return false;
-    }
+    } // A, AA, AAA, AAAA, AAAAA => false, AAAAE
 }
