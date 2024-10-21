c58899b75da6f5ec7bfecc9f03bd26f8e3edd561
hyeonbin <90017767+hyeonbinn@users.noreply.github.com>
2024-10-21 13:34:38 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1392 ms, Memory: 318120 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,51 +1,42 @@
-//package org.example.DfsBfs;
-
 import java.util.Scanner;
 
 public class Main {
-
-    static int N,M;
-    static int[][] Graph;
-    static boolean[] Visited;
+    static int N, M;
+    static int[][] graph;
+    static boolean[] visited;
     static int count;
-
-    public static void dfs(int node) {
-        Visited[node] = true;
-
-        for (int i = 1; i < N+1; i++) { //N이 1부터이므로 범위 설정 주의!!
-            if(!Visited[i] && Graph[node][i] == 1)
-                dfs(i);;
-        }
-    }
-
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
 
         N = in.nextInt();
         M = in.nextInt();
 
-        //좌표를 그대로 받아들이기 위해 N+1 크기로 선언.
-        Graph = new int[N+1][N+1];
-        Visited = new boolean[N+1];
+        graph = new int[N+1][N+1];
+        visited = new boolean[N+1];
 
-        for (int i = 0; i < M; i++) {
+        for(int i = 0; i < M; i++) {
             int u = in.nextInt();
             int v = in.nextInt();
-            Graph[u][v] = Graph[v][u] =  1;
+            graph[u][v] = graph[v][u] = 1;
         }
 
-        count = 0; //count 초기화 선언 위치 주의
-
-        for (int i = 1; i < N+1; i++){ //N범위 주의..!
-            if(!Visited[i]){
-                dfs(i);
+        count = 0;
+        for(int i = 1; i < N+1; i++) {
+            if(!visited[i]) {
+                DFS(i);
                 count++;
             }
         }
-
         System.out.println(count);
-
     }
 
+    public static void DFS(int node) {
+        visited[node] = true;
+        for(int i = 1; i <= N; i++) {
+            if(!visited[i] && graph[node][i] == 1) {
+                DFS(i);
+            }
+        }
+
+    }
 }
